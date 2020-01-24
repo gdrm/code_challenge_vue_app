3ac7114fb5b2cfeaa3830010a21c03b619b0976e
Gerardo Rocha <daniel0416@gmail.com>
2020-01-24 14:54:22 -0600

    Add packages

--- src/main.js
+++ src/main.js
@@ -5,7 +5,22 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import axios from 'axios'
 
+// load prism somewhere
+import 'prismjs'
+import 'prismjs/themes/prism-tomorrow.css'
+// vue-prism-editor dependency
+import 'vue-prism-editor/dist/VuePrismEditor.css'
+import 'prismjs/components/prism-ruby.min'
+import 'prismjs/components/prism-java.min'
+import 'prismjs/components/prism-c.min'
+
 Vue.config.productionTip = false
+
+const token = localStorage.getItem('token')
+
+if (token) {
+  axios.defaults.headers.common['AUTHORIZATION'] = token
+}
 
 new Vue({
   router,
