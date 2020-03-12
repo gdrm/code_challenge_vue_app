5e64b9b5f715512d2ff0e0d4fdcdffe1f9d48eec
Gerardo Rocha <daniel0416@gmail.com>
2020-03-12 11:40:13 -0600

    Use vuex-persistedstate and add challenge to vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import user from '@/store/modules/user'
+import challenges from '@/store/modules/challenges'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
   },
+  plugins: [createPersistedState()],
   mutations: {
   },
   actions: {
   },
   modules: {
-    user
+    user,
+    challenges
   }
 })
